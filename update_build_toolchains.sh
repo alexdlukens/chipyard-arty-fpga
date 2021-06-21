e8ffb09188fc5a0d49729bbe167badb56834917e
Alexander Lukens <alexdlukens@gmail.com>
2021-06-21 15:34:13 -0500

    added new script to add required env variables to build_toolchains script

--- update_build_toolchains.sh
+++ update_build_toolchains.sh
@@ -12,7 +12,13 @@ CHIPYARD_DIR=$(pwd)/../..
 BUILD_TOOLCHAINS=${CHIPYARD_DIR}/scripts/build-toolchains.sh
 
 #edit build_toolchians.sh to support multilib
-sed 
+sed -i 's/--enable-multilib/ /g ' $BUILD_TOOLCHAINS
+
+sed -i 's/module_build riscv-gnu-toolchain --prefix="${RISCV}" --with-cmodel=medany/module_build riscv-gnu-toolchain --prefix="${RISCV}" --with-cmodel=medany --enable-multilib/g' $BUILD_TOOLCHAINS
+
+#add additional variables to Chipyard ENV files to support Freedom E SDK
+grep -qF 'RISCV_PATH' $BUILD_TOOLCHAINS || sed -i '/${LD_LIBRARY_PATH}"}/ a \\n#added by update_build_toolchains script \nexport RISCV_PATH=${RISCV} \nexport RISCV_OPENOCD_PATH=${RISCV} \nexport FREEDOM_SDK=/home/alex/Desktop/chipThings/freedom-e-sdk \nexport FPGA_PATH=${RISCV}/../fpga' $BUILD_TOOLCHAINS
+
 
 #return to starting dir
 cd -
