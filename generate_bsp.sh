2491befc78f3bd1cf4e38d007d69e7ac83287284
Alexander Lukens <alexdlukens@gmail.com>
2021-06-22 01:21:38 -0500

    successfully generates files for default arty project

--- generate_bsp.sh
+++ generate_bsp.sh
@@ -18,11 +18,15 @@ cd "$(dirname "$0")"
 if [ ! -f "${GENERATED_DTS}" ]; then
 	echo "Generated DTS file does not exist. Ensure the \"LONG_NAME\" variable is set correctly in this script";
 	exit 1;
-
+fi
 if [ ! -d "${FREEDOM_SDK}/bsp/chipyard_arty" ]; then
 	mkdir ${FREEDOM_SDK}/bsp/chipyard_arty;
 fi
 
-#
+#Use Freedom E SDK 'update-targets.sh' script to generate board files
+cd ${FREEDOM_SDK}/bsp
+./update-targets.sh --target-name chipyard_arty --target-type arty --sdk-path=${FREEDOM_SDK} --target-dts=${GENERATED_DTS} || exit 1;
+
+echo "File Generation Complete. Find the created files in ${FREEDOM_SDK}/bsp/chipyard_arty"
 
 cd ${EXIT_DIR}
