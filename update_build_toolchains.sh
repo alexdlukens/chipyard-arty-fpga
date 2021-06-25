b1f536d8bf4f9bbe4a02a37781fa6c71dd638814
Alexander Lukens <alexdlukens@gmail.com>
2021-06-25 00:35:18 -0500

    revised script

--- update_build_toolchains.sh
+++ update_build_toolchains.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#Script to enable multilib support to RISCV-GNU-Toolchain in Chipyard
+#Script to enable multilib support in RISCV-GNU-Toolchain in Chipyard. Useful for using Freedom-E-SDK to compile programs for Arty FPGA boards
 #Alexander Lukens 06/21/2021
 
 #Move to script directory
+starting_dir=$(pwd)
 cd "$(dirname "$0")"
 
 #Path to Chipyard Base DIR
@@ -11,6 +12,9 @@ CHIPYARD_DIR=$(pwd)/../..
 #Path to 'build-toolchains.sh'
 BUILD_TOOLCHAINS=${CHIPYARD_DIR}/scripts/build-toolchains.sh
 FREEDOM_SDK_PATH=${1:-$(readlink -f $(pwd)/../../../freedom-e-sdk)}
+
+echo "This script is intended to enable multilib support in the RISCV GNU Toolchain to allow compiling
+programs for Arty FPGA board implementations in Chipyard"
 
 if [ $# -eq 0 ];
 then
@@ -20,7 +24,7 @@ then
 fi
 
 #edit build_toolchians.sh to support multilib
-sed -i 's/--enable-multilib/ /g ' $BUILD_TOOLCHAINS
+sed -i 's/--enable-multilib//g ' $BUILD_TOOLCHAINS
 
 sed -i 's/module_build riscv-gnu-toolchain --prefix="${RISCV}" --with-cmodel=medany/module_build riscv-gnu-toolchain --prefix="${RISCV}" --with-cmodel=medany --enable-multilib/g' $BUILD_TOOLCHAINS
 
@@ -31,10 +35,19 @@ sed -i -e '/#added by update_build/, +4d' $BUILD_TOOLCHAINS
 sed -i '/${LD_LIBRARY_PATH}/{N;s/\n$//}' $BUILD_TOOLCHAINS
 
 #add additional variables to Chipyard ENV files to support Freedom E SDK
-sed -i '/${LD_LIBRARY_PATH}"}/ a \\n#added by update_build_toolchains script \nexport RISCV_PATH=${RISCV} \nexport RISCV_OPENOCD_PATH=${RISCV} \nexport FPGA_PATH=${RISCV}/../fpga' $BUILD_TOOLCHAINS
+sed -i '/${LD_LIBRARY_PATH}"}/ a \\n#added by update_build_toolchains script \nexport RISCV_PATH=${RISCV} \nexport FPGA_PATH=${RISCV}/../fpga' $BUILD_TOOLCHAINS
 
 sed -i "/{RISCV}\/..\/fpga/ a export FREEDOM_SDK=$FREEDOM_SDK_PATH" $BUILD_TOOLCHAINS
 
+ 
+while true; do
+    read -p "Script changes applied, recompile toolchain? (this may take a while) [y/n]" yn
+    case $yn in
+        [Yy]* ) ${CHIPYARD_DIR}/scripts/build-toolchains.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 #return to starting dir
-cd -
+cd $starting_dir
